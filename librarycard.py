d7e0c903f6c0ab6176f6118c98928ad3c6fc1dcd
Grissess <grissess@nexusg.org>
2024-03-23 12:22:26 -0400

    Fix pagination of nominations
    Previously, all nominations were displayed on the first page, with blank
    pages following. This change introduces an islice for the iterable that
    should match the expected pagination strategy.
    
    It should be noted that Py3.12 introduces itertools.batched, which
    approximates this; however, this commit does not presume to change
    Py3.12 to the minimum necessary version.

--- librarycard.py
+++ librarycard.py
@@ -1,4 +1,5 @@
 import discord
+import itertools
 import lib.goodreads as goodreads
 import lib.royalroad as royalroad
 import os
@@ -830,7 +831,8 @@ async def listNominations(ctx, past_sessions: Option(int, "How many prior sessio
 
   found = nominateSessions.aggregate(search)
 
-  pagecount = math.ceil((count/10))
+  page_size = 10
+  pagecount = math.ceil((count/page_size))
 
   pages = []
 
@@ -839,7 +841,7 @@ async def listNominations(ctx, past_sessions: Option(int, "How many prior sessio
     itemList = ""
     bookCount = 1
 
-    for b in found:
+    for b in itertools.islice(found, page_size):
       itemList += "\n{}. {}".format(bookCount, pascal_case(str(b['_id'])))
       bookCount += 1
       
